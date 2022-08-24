2a2a5c88630ebaca2aec1bf1b6158a00397ef80f
RutujaDevarakki <103737743+RutujaDevarakki@users.noreply.github.com>
2022-08-24 15:52:05 +0530

    Add files via upload

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace test1
+{
+   public class test1
+
+    {
+        public static void Main()
+        {
+            string name;
+            Console.WriteLine("Enter your name ");
+            name = Console.ReadLine();
+            Console.WriteLine($"Your name {name}");
+        }
+    }
+}
